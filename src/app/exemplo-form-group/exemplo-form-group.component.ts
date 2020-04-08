3392a7dbed99fd4a683c3e646d42f54d3cf5cd5f
Marcos Latchuk <Marcos_Latchuk@hotmail.com>
2020-04-08 18:28:30 -0300

    arrumado

--- src/app/exemplo-form-group/exemplo-form-group.component.ts
+++ src/app/exemplo-form-group/exemplo-form-group.component.ts
@@ -3,52 +3,44 @@ import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { AppService } from '../app.service';
 
 @Component({
-    selector: 'app-exemplo-form-group',
-    templateUrl: './exemplo-form-group.component.html',
-    styleUrls: ['./exemplo-form-group.component.scss']
+  selector: 'app-exemplo-form-group',
+  templateUrl: './exemplo-form-group.component.html',
+  styleUrls: ['./exemplo-form-group.component.scss']
 })
 export class ExemploFormGroupComponent implements OnInit {
 
-    // formulario = new FormGroup({
-    //   nome: new FormControl(null, [Validators.required, Validators.maxLength(32)]),
-    //   sobrenome: new FormControl(null, [Validators.required, Validators.maxLength(16)]),
-    //   email: new FormControl(null, [Validators.required, Validators.email]),
-    //   senha: new FormControl(null, [Validators.required, Validators.minLength(6), Validators.maxLength(16)]),
-    //   receberNovidades: new FormControl(false)
-    // });
+  formulario = new FormGroup({
+    nome: new FormControl(null, [Validators.required, Validators.maxLength(32)]),
+    sobrenome: new FormControl(null, [Validators.required, Validators.maxLength(16)]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    senha: new FormControl(null, [Validators.required, Validators.minLength(6), Validators.maxLength(16)]),
+    receberNovidades: new FormControl(false)
+  });
 
-    formulario = new FormGroup({
-        data1: new FormControl(null),
-        data2: new FormControl(null),
-        nome: new FormControl(null),
-    });
+  esconderSenha = true;
 
-    esconderSenha = true;
+  jsonDados: string;
 
-    jsonDados: string;
+  constructor(private appService: AppService) { }
 
-    constructor(private appService: AppService) { }
+  ngOnInit(): void {
 
-    ngOnInit(): void {
-        this.formulario.controls['nome'].setValue(this.appService.teste);
+  }
+
+  limpar() {
+    this.jsonDados = null;
+  }
+
+  enviar() {
+
+    if (this.formulario.invalid) {
+      return;
     }
 
-    limpar() {
-        this.jsonDados = null;
-    }
+    const dados = this.formulario.value;
 
-    enviar() {
+    this.jsonDados = JSON.stringify(dados);
 
-        this.appService.teste = this.formulario.controls['nome'].value;
-
-        if (this.formulario.invalid) {
-            return;
-        }
-
-        const dados = this.formulario.value;
-
-        this.jsonDados = JSON.stringify(dados);
-
-    }
+  }
 
 }
